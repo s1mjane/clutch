da01fa84c85a18f14869c92df56c76e13078b0b1
s1mjane <s1mjane@handong.ac.kr>
2024-07-01 17:23:18 +0900

    0701

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:cluth_vscode_ver1/widget/tab_screen.dart';
-import 'package:table_calendar/table_calendar.dart'; // TabScreen 경로 확인
+// import 'package:cluth_vscode_ver1/widget/tab_screen.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,17 +36,15 @@ class CombinedApp extends StatelessWidget {
         highlightColor: Colors.white,
       ),
       home: DefaultTabController(
-        length: 5, // 탭의 수
+        length: 3, // 탭의 수
         child: Scaffold(
           appBar: AppBar(
             title: const Text('Clutch DEMO'),
             bottom: const TabBar(
               tabs: [
-                Tab(text: '슛맵'),
-                Tab(text: '포메이션 맵'),
-                Tab(text: '패스 맵'),
-                Tab(text: '최근'),
-                Tab(text: '과거')
+                Tab(text: '요약'),
+                Tab(text: 'Pitch'),
+                Tab(text: '주요통계'),
               ],
             ),
           ),
@@ -54,17 +52,22 @@ class CombinedApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Spacer(),
-                Divider(),
-                Text('Multiple choice'),
-                MultipleChoice(),
-                Divider(),
-                Spacer(),
+                const Text('Segmented Button'),
+                const Row(children: [
+                  TimeChoice(),
+                  Spacer(),
+                  ContentChoice(),
+                ],),
+                const Divider(),
                 Container(
                     child: TableCalendar(
+                        locale: "en_US",
+                        rowHeight: 43,
+                        headerStyle: HeaderStyle(formatButtonVisible: false, titleCentered: true),
+                        availableGestures: AvailableGestures.all,
                         focusedDay: DateTime.now(),
                         firstDay: DateTime.utc(2010, 3, 14),
-                        lastDay: DateTime.utc(2030, 10, 16))),
+                        lastDay: DateTime.utc(2030, 10, 16)))
               ],
             ),
           ),
@@ -74,38 +77,64 @@ class CombinedApp extends StatelessWidget {
   }
 }
 
-enum Sizes { extraSmall, small, medium, large, extraLarge }
+enum Times { past, present }
 
-class MultipleChoice extends StatefulWidget {
-  const MultipleChoice({super.key});
+class TimeChoice extends StatefulWidget {
+  const TimeChoice({super.key});
 
   @override
-  State<MultipleChoice> createState() => _MultipleChoiceState();
+  State<TimeChoice> createState() => _TimeChoiceState();
 }
 
-class _MultipleChoiceState extends State<MultipleChoice> {
-  Set<Sizes> selection = <Sizes>{Sizes.large, Sizes.extraLarge};
+class _TimeChoiceState extends State<TimeChoice> {
+  Set<Times> selection = <Times>{Times.present};
 
   @override
   Widget build(BuildContext context) {
-    return SegmentedButton<Sizes>(
-      segments: const <ButtonSegment<Sizes>>[
-        ButtonSegment<Sizes>(value: Sizes.extraSmall, label: Text('XS')),
-        ButtonSegment<Sizes>(value: Sizes.small, label: Text('S')),
-        ButtonSegment<Sizes>(value: Sizes.medium, label: Text('M')),
-        ButtonSegment<Sizes>(
-          value: Sizes.large,
-          label: Text('L'),
-        ),
-        ButtonSegment<Sizes>(value: Sizes.extraLarge, label: Text('XL')),
+    return SegmentedButton<Times>(
+      segments: const <ButtonSegment<Times>>[
+        ButtonSegment<Times>(value: Times.past, label: Text('과거')),
+        ButtonSegment<Times>(value: Times.present, label: Text('현재')),
       ],
       selected: selection,
-      onSelectionChanged: (Set<Sizes> newSelection) {
+      onSelectionChanged: (Set<Times> newSelection) {
         setState(() {
           selection = newSelection;
         });
       },
-      multiSelectionEnabled: true,
+      multiSelectionEnabled: false,
     );
   }
 }
+
+enum Contents { shoot, formation, pass }
+
+class ContentChoice extends StatefulWidget {
+  const ContentChoice({super.key});
+
+  @override
+  State<ContentChoice> createState() => _ContentChoiceState();
+}
+
+class _ContentChoiceState extends State<ContentChoice> {
+  Set<Contents> selection = <Contents>{Contents.shoot};
+
+  @override
+  Widget build(BuildContext context) {
+    return SegmentedButton<Contents>(
+      segments: const <ButtonSegment<Contents>>[
+        ButtonSegment<Contents>(value: Contents.shoot, label: Text('슛맵')),
+        ButtonSegment<Contents>(value: Contents.formation, label: Text('포메이션 맵')),
+        ButtonSegment<Contents>(value: Contents.pass, label: Text('패스 맵')),
+      ],
+      selected: selection,
+      onSelectionChanged: (Set<Contents> newSelection) {
+        setState(() {
+          selection = newSelection;
+        });
+      },
+      multiSelectionEnabled: false,
+    );
+  }
+}
+
