425850cbb9a7fc57c37799e1ded7ed18b9486991
s1mjane <s1mjane@handong.ac.kr>
2024-07-01 15:12:29 +0900

    tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,43 @@
+// import 'package:cluth_vscode_ver1/widget/tab_screen.dart';
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'CLUTCH_0701',
+//       theme: ThemeData(
+//         brightness: Brightness.dark,
+//         primaryColor: Colors.black,
+//         highlightColor: Colors.white,
+//       ),
+//       home: TabScreen(),  // TabScreen을 홈으로 설정
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:cluth_vscode_ver1/widget/bottom_bar.dart';
+import 'package:cluth_vscode_ver1/widget/tab_screen.dart';  // 경로 확인
 
 void main() => runApp(const MyApp());
 
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'simflix',
+      title: 'CLUTCH_0701',
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.black,
         highlightColor: Colors.white,
       ),
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          body: TabBarView(
-            physics: const NeverScrollableScrollPhysics(),
-            children: <Widget>[
-              Container(),
-              Container(),
-              Container(),
-              Container(),
-            ],
-          ),
-          bottomNavigationBar: Bottom(),
-        ),
-      ),
+      home: TabScreen(),  // TabScreen을 홈으로 설정
     );
   }
 }
---
+++ lib/widget/tab_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TabScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+          title: const Text('Tabs Demo'),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text("요약")),
+            Center(child: Text("Pitch")),
+            Center(child: Text("주요통계")),
+          ],
+        ),
+      ),
+    );
+  }
+}
